f4c006b00f30340130b994b95b224edcaa880e75
Igor <igor.mamaev1@gmail.com>
2025-11-10 12:24:08 +0300

    add bot

---
+++ bot/handlers/connection_test.py
@@ -0,0 +1,27 @@
+import requests
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+# noinspection PyUnresolvedReferences
+from settings import settings
+
+router = Router()
+
+
+@router.message(Command("dbg_test_backend_connection"))
+async def connection_test(message: Message) -> None:
+    url = f"{settings.BACKEND_URL}/api/ping/"
+    try:
+        response: requests.Response = requests.get(url=url)
+        assert isinstance(response, requests.Response), f"server returned {type(response)} instead of Response"
+    except Exception as e:
+        await message.answer(text=(
+            "Внутренняя ошибка при попытке соединения с бэкендом:\n"
+            f"{e}"
+        ))
+        return
+    await message.answer(text=(
+        f"Проверка соединения с бэкендом.\n"
+        f"URL: {url}\n"
+        f"StatusCode: {response.status_code}"
+    ))
---
+++ bot/settings.py
@@ -0,0 +1,21 @@
+﻿from typing import Set
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "dndbot"
+    LOG_LEVEL: str = "INFO"
+
+    BOT_TOKEN: str
+    ADMIN_IDS: Set[int]
+
+    BACKEND_URL: str = "http://backend:8000"
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_PASSWORD: str
+    REDIS_DB: int
+
+
+settings = Settings()
